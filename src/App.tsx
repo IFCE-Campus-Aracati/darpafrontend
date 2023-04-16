02d69025d6073d884757e7a933c34a4960af76fb
Talison Maia <talison.maia@gmail.com>
2023-04-15 22:04:49 -0300

    adicionado pagina de login e cadastro + fontes

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,28 @@
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import 'normalize.css';
-import './index.css';
+import './index.css'
 import SolicitationButton from './components/SolicitationButton'
 import SearchBar from './components/SearchBar';
 import EditButton from './components/EditButton';
 import DeleteButton from './components/DeleteButton';
 import ConfirmButton from './components/ConfirmButton';
 import CancelButton from './components/CancelButton';
+import LoginPg from './pages/Login'
+import CreateAccountPg from './pages/CreateAccount';
+
+
 
 function App() {
   return (
     <>
     <div className="container">
-      <Header
-        username="User"
-        onLogout={() => {
-          throw new Error('Function not implemented.');
-        }}
-      />
-      <Sidebar isOpen={false} onClose={function (): void {
-        throw new Error('Function not implemented.');
-      } } />
-      <SearchBar onSearch={() => console.log('falta implementar o resto')}/>
-      <SolicitationButton onClick={() => console.log("Click")}/>
-      <EditButton onClick={() => console.log("editando")}/>
-      <DeleteButton onClick={() => console.log("excluindo")}/>
-      <ConfirmButton onClick={()=> console.log("confirm")}/>
-      <CancelButton onClick={()=> console.log("cancelar")}/>
+      
+{//      <LoginPg onLogout={()=> console.log("")}/>
+}
+      <CreateAccountPg onLogout={()=> console.log("")}/>
       
     </div>
-    
-      
-    
-
-  
    
     </>
     
