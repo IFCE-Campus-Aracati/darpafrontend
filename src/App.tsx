de9ba266dc0cecef910934d4ce1bb9daae703fc0
Matheus Matias <matheus.matias.viana@hotmail.com>
2023-04-16 12:23:08 -0300

    clean app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import 'normalize.css';
 import './index.css';
 
-import Header from './components/Header';
-import PrintRequestsTable from './components/PrintTable';
-import Sidebar from './components/Sidebar';
 import UserHome from './pages/UserHome';
 
 function App() {
